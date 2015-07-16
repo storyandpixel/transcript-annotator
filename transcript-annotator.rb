366688ed28409ef01b5b5292e9469f4f1ebd437a
Jedidiah Hurt <jed.hurt@gmail.com>
2015-07-16 14:20:18 -0600

    Usage instructions and bugfixes

--- transcript-annotator.rb
+++ transcript-annotator.rb
@@ -3,7 +3,10 @@ require 'bundler'
 Bundler.require
 require 'pry'
 
-# Usage: transcript-annotator.rb "Jake Lodwick" "jake lodwick app story 1.0.xml" "jake lodwick app story 1.0.md"
+unless ARGV.length == 3
+  puts 'Usage: transcript-annotator.rb "Jake Lodwick" "jake lodwick app story 1.0.xml" "jake lodwick app story 1.0.md"'
+  exit(1)
+end
 
 #####################
 # Config
@@ -11,9 +14,9 @@ require 'pry'
 FrameRate = 24
 MaxQuoteLength = 2000
 
-interviewee_name = ARGV[0]
-sequence_xml_path = ARGV[1]
-markdown_path = ARGV[2]
+IntervieweeName = ARGV[0]
+SequenceXmlPath = ARGV[1]
+MarkdownPath = ARGV[2]
 
 #####################
 # Functions
@@ -27,11 +30,15 @@ all_containing = containing.curry.(:find_all)
 def format_quote_header(interviewee_name, clip)
   in_point = frames_to_formatted_timestamp(clip, 'start', FrameRate)
   out_point = frames_to_formatted_timestamp(clip, 'end', FrameRate)
-   "#{interviewee_name} (#{in_point}-#{out_point})"
+  header(interviewee_name, in_point, out_point)
 end
 
 def frames_to_formatted_timestamp(clip, node_name, frame_rate)
   remove_leading_zeroes_from_timestamp(seconds_to_timestamp(clip.css(node_name).text.to_i / frame_rate))
+end
+
+def header(name, in_point='', out_point='')
+  "#{name} (#{in_point}-#{out_point})"
 end
 
 def highlighted_quotes(html, highlight_tag='strong')
@@ -61,7 +68,7 @@ def split_long_quotes(text, char_limit)
   excerpt, leftover = truncate_on_word_boundary(text, char_limit)
   [excerpt, split_long_quotes(leftover, char_limit)].
     flatten.
-    join("\n---\n".colorize(:red))
+    join("\n***[BREAK]***\n")
 end
 
 def truncate_on_word_boundary(text, char_limit)
@@ -75,6 +82,10 @@ def truncate_on_word_boundary(text, char_limit)
 end
 
 def zip_quote_headers_and_bodies(headers, bodies)
+  if headers.length < bodies.length
+    off_by = bodies.length - headers.length
+    off_by.times { headers.push(header(IntervieweeName)) }
+  end
   headers.
     zip(bodies).
     map { |pair| pair.join(' ') }
@@ -83,7 +94,7 @@ end
 #####################
 # Implementation
 #####################
-project = Nokogiri::XML(File.read(sequence_xml_path))
+project = Nokogiri::XML(File.read(SequenceXmlPath))
 
 sequence = project.css('sequence')
 
@@ -91,18 +102,18 @@ golden_clips = all_containing.(sequence.css('media video track').first.css('clip
                                'labels label2',
                                'Mango')
 
-quote_headers = golden_clips.map { |gc| format_quote_header(interviewee_name, gc) }
-quote_bodies = highlighted_quotes(markdown_path_to_html(markdown_path))
+quote_headers = golden_clips.map { |gc| format_quote_header(IntervieweeName, gc) }
+quote_bodies = highlighted_quotes(markdown_path_to_html(MarkdownPath))
 
 if quote_headers.length != quote_bodies.length
-  puts "~> Woops. We don't have the same number of marked clips and marked quotes.".colorize(:red)
-  puts "~> The output will need some massaging to resolve this.".colorize(:red)
-  puts "     Clips: #{quote_headers.length}".colorize(:red)
-  puts "    Quotes: #{quote_bodies.length}".colorize(:red)
-  puts "==========================================================================\n\n".colorize(:red)
+  puts "~> Woops. We don't have the same number of marked clips and marked quotes."
+  puts "~> The output will need some massaging to resolve this."
+  puts "     Clips: #{quote_headers.length}"
+  puts "    Quotes: #{quote_bodies.length}"
+  puts "================================================================\n\n"
 end
 
 zipped = zip_quote_headers_and_bodies(quote_headers, quote_bodies)
 puts zipped.
   map { |quote| split_long_quotes(quote, MaxQuoteLength) }.
-  join("\n\n\n\n=======================================================================\n\n\n\n".colorize(:green))
+  join("\n\n\n\n================================================================\n\n\n\n")
